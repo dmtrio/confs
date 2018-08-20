5bcb84cad1c5420d344139c986d7d2b72e653131
Demetrio Urquidi <demetrio_urquidi@homedepot.com>
2018-08-20 11:02:34 -0500

    fixed broken terminal branch naming

--- bash_profiles/modules/git_branch.sh
+++ bash_profiles/modules/git_branch.sh
@@ -1,6 +1,6 @@
 # get current branch in git repo
-    BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
 function parse_git_branch() {
+    BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
     if [ ! "${BRANCH}" == "" ]
     then
         STAT=`parse_git_dirty`
