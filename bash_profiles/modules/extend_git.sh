fc4092f00c3177f1ab99568c7a34fd43f7f2691d
Demetrio <dmitr77@gmail.com>
2018-08-20 22:23:36 -0500

    added --branch to refence currenct branch

--- bash_profiles/modules/extend_git.sh
+++ bash_profiles/modules/extend_git.sh
@@ -1,4 +1,19 @@
 function cgit () {
+
+  local BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+  local NPA=()
+  for PARAM in "${@}"
+  do
+    case "$PARAM" in
+      --branch)
+        PARAM="$BRANCH"
+        ;;
+    esac
+    NPA+=("$PARAM")
+  done
+
+
   case "$1" in
     pull)
       case "$2" in
@@ -6,20 +21,19 @@ function cgit () {
           gitPullOverwrite $3
           ;; 
         *) 
-          git "$@"
+          git "${NPA[@]}"
           ;;
       esac 
       ;; 
     *) 
-      git "$@"
+      git "${NPA[@]}"
       ;;
-  esac 
+  esac
 
 
   function gitPullOverwrite() {
+    local REMOTE=$1 
     if verifyParamExists $1; then
-      local REMOTE=$1 
-      local BRANCH=$(git rev-parse --abbrev-ref HEAD)
       if confirm "This OVERWRITES LOCAL COMMITS not in ${REMOTE}/${BRANCH}. Is this ok?"; then
         git fetch $REMOTE
         git reset $REMOTE/$BRANCH --hard
