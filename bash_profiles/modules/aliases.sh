1b1910e93d6f145ed83fd7bf130d0b6651e734d9
Demetrio Urquidi <demetrio_urquidi@homedepot.com>
2018-08-22 11:25:46 -0500

    adding build functions

--- bash_profiles/modules/aliases.sh
+++ bash_profiles/modules/aliases.sh
@@ -6,3 +6,6 @@ alias bp='nano ~/.bash_profile'
 alias ngre='sudo brew services restart nginx'
 alias ngs='sudo brew services stop nginx'
 alias ngconf='code /usr/local/etc/nginx/nginx.conf'
+
+# git
+alias repo=$(basename `git rev-parse --show-toplevel`)
