1d8ff6893594655d8e67298740777086766f7d1c
Demetrio Urquidi <DEMETRIO_URQUIDI@homedepot.com>
2018-11-27 14:10:54 -0600

    added vscode to PATH

--- bash_profiles/modules/default_home.sh
+++ bash_profiles/modules/default_home.sh
@@ -4,6 +4,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# Add Visual Studio Code (code)
+export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
 # directories, builds, and other shortcuts to run git projects go here
 function dev() {
   local GITPROJECTSPATH="~/git"
